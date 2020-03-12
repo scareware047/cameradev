71c176a2756f2e2da05ab13daeffebfccbbdcce6
thetejaspandey <pandeyt@tcd.ie>
2020-03-12 21:05:22 +0000

    Added argparse support.

--- default.py
+++ default.py
@@ -4,10 +4,16 @@ Author: Tejas Pandey
 
 import cv2
 import time
+import argparse
+
 
 if __name__ == "__main__":
 
-    cap = cv2.VideoCapture(0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--src", default=0)
+    args = parser.parse_args()
+
+    cap = cv2.VideoCapture(args.src)
     fps = 0
     font = cv2.FONT_HERSHEY_SIMPLEX
     start = time.time()
@@ -16,13 +22,16 @@ if __name__ == "__main__":
         _, frame = cap.read()
 
         curr_time = time.time() - start
+        frame = cv2.resize(frame, (720, 480))
         cv2.putText(frame, 'FPS: {}'.format(round(fps/curr_time)),
                     (230, 50), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.imshow("Display", frame)
+        cv2.imshow("Default", frame)
         fps += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+print("Total Frames: {}".format(fps))
+print("Time Taken: {:.2f}".format(time.time()-start))
 cap.release()
 cv2.destroyAllWindows()
