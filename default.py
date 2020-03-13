de2cd9304f6f80d8df0744c734783831f3562116
tejaspandey <pandeyt@tcd.ie>
2020-03-13 18:47:35 +0000

    added documentation

--- default.py
+++ default.py
@@ -1,4 +1,6 @@
 """
+Script to run video stream using OpenCV on a single thread.
+
 Author: Tejas Pandey
 """
 
@@ -8,38 +10,48 @@ import argparse
 
 
 if __name__ == "__main__":
-
+    # Parse arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src", default=0)
-    parser.add_argument("--benchmark", action='store_true')
+    parser.add_argument("--src", default=0,
+                        help="Path to src file. Defaults to camera.")
+    parser.add_argument("--benchmark", action='store_true',
+                        help="Run for 60 seconds.")
     args = parser.parse_args()
 
+    # Initialize camera and metrics.
     cap = cv2.VideoCapture(args.src)
-    fps = 0
+    frames = 0
     font = cv2.FONT_HERSHEY_SIMPLEX
     start = time.time()
 
+    # Run infinte loop.
     while True:
+        # Read frame.
         grabbed, frame = cap.read()
-
+        # Break if no frame.
         if grabbed is False:
             break
-
-        curr_time = time.time() - start
+        # Resize frame.
         frame = cv2.resize(frame, (720, 480))
-        cv2.putText(frame, 'FPS: {}'.format(round(fps/curr_time)),
+        # Calculate fps.
+        frames += 1
+        curr_time = time.time() - start
+        fps = round(frames/curr_time)
+        # Display image with FPS.
+        cv2.putText(frame, 'FPS: {}'.format(fps),
                     (230, 50), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.imshow("Default", frame)
-        fps += 1
-
+        # Display frame for 1 ms and break if user presses 'q'.
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        # Run for 60 seconds if benchmark.
         if args.benchmark and curr_time > 60:
             break
 
-    print("Total Frames: {}".format(fps))
+    # Print metrics.
+    print("Total Frames: {}".format(frames))
     print("Time Taken: {:.2f}".format(time.time()-start))
-    print("FPS: {}".format(round(fps/curr_time)))
+    print("FPS: {}".format(fps))
+    # Clean up.
     cap.release()
     cv2.destroyAllWindows()
--- multi_threaded.py
+++ multi_threaded.py
@@ -1,4 +1,7 @@
 """
+Script to run and display video stream using OpenCV.
+Asynchronus implementation using queues.
+
 Author: Tejas Pandey
 """
 
@@ -8,16 +11,38 @@ import threading
 import argparse
 from queue import Queue
 
+
 class AsyncVideoCapture:
+    """
+    Asynchronus video capture.
+    """
+
     def __init__(self, src):
+        """
+        Initialize camera capture from src.
+        Creates a queue to keep all frames.
+
+        Args:
+            src: Input source.
+        """
+
         self.cap = cv2.VideoCapture(src)
         self.running = False
         self.Q = Queue(256)
 
     def __exit__(self, exec_type, exc_value, traceback):
+        """
+        Release capture object on exit.
+        """
+
         self.cap.release()
 
     def start(self):
+        """
+        Start streaming from camera.
+        Create thread to read and push frames to queue.
+        """
+
         if self.running:
             return None
         self.running = True
@@ -27,6 +52,11 @@ class AsyncVideoCapture:
         return self
 
     def update(self):
+        """
+        Push frames to queue every read.
+        If queue is full, put thread to sleep.
+        """
+
         while self.running:
             if not self.Q.full():
                 grabbed, frame = self.cap.read()
@@ -35,48 +65,64 @@ class AsyncVideoCapture:
                 time.sleep(0.1)
 
     def read(self):
+        """
+        Pop frames from queue.
+        """
+
         return self.Q.get()
-        
+
     def release(self):
+        """
+        Clean up and release memory.
+        """
+
         self.running = False
         self.thread.join()
         self.cap.release()
 
 
 if __name__ == "__main__":
-
+    # Parse arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument("--src", default=0)
     parser.add_argument("--benchmark", action='store_true')
     args = parser.parse_args()
 
+    # Initialize camera and metrics.
     cap = AsyncVideoCapture(args.src)
     cap.start()
-    fps = 0
+    frames = 0
     font = cv2.FONT_HERSHEY_SIMPLEX
     start = time.time()
 
+    # Run infinte loop.
     while True:
+        # Read frame.
         grabbed, frame = cap.read()
-
+        # Break if no frame.
         if grabbed is False:
             break
-
-        curr_time = time.time() - start
+        # Resize frame.
         frame = cv2.resize(frame, (720, 480))
-        cv2.putText(frame, 'FPS: {}'.format(round(fps/curr_time)),
+        # Calculate fps.
+        frames += 1
+        curr_time = time.time() - start
+        fps = round(frames/curr_time)
+        # Display image with FPS.
+        cv2.putText(frame, 'FPS: {}'.format(fps),
                     (230, 50), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.imshow("Multi-Threaded", frame)
-        fps += 1
-
+        # Display frame for 1 ms and break if uses presses 'q'.
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        # Run for 60 seconds if benchmark.
         if args.benchmark and curr_time > 60:
             break
 
-    print("Total Frames: {}".format(fps))
+    # Print metrics.
+    print("Total Frames: {}".format(frames))
     print("Time Taken: {:.2f}".format(time.time()-start))
-    print("FPS: {}".format(round(fps/curr_time)))
+    print("FPS: {}".format(fps))
+    # Clean up.
     cap.release()
     cv2.destroyAllWindows()
